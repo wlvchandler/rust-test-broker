f32b6d3293c7267ddbe8f3bf117d42de6718c979
will chandler <wlvchandler@gmail.com>
2024-11-04 14:29:18 -0500

    get networking working minimally

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,30 +1,41 @@
 use broker::net::BrokerClient;
 use std::time::Instant;
+use std::io::Write;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = BrokerClient::connect("127.0.0.1:7878").await?;
-    
     const MESSAGE_SIZE: usize = 1024;
-    const ITERATIONS: usize = 1_000_000;
+    const ITERATIONS: usize = 10_000;  // reduced for testing
     
+    println!("Connecting to broker...");
+    let mut client = BrokerClient::connect("127.0.0.1:7878").await?;
     println!("connected");
-    println!("sending: msg_n:{} bytes:{}...", ITERATIONS, MESSAGE_SIZE);
     
     let data = vec![1u8; MESSAGE_SIZE];
     let start = Instant::now();
     
     for i in 0..ITERATIONS {
-        if i % (ITERATIONS / 10) == 0 {
-            print!("{}%...", (i * 100) / ITERATIONS);
-            let _ = std::io::Write::flush(&mut std::io::stdout());
+        if i % 100 == 0 {  // More frequent updates
+            print!("\rProgress: {:.1}%", (i as f64 / ITERATIONS as f64) * 100.0);
+            std::io::stdout().flush()?;
         }
-        
-        client.send(&data).await?;
+
+        match client.send(&data).await {
+            Ok(_) => {
+                if i % 1000 == 0 {
+                    println!("\nSent {} messages", i);
+                }
+            }
+            Err(e) => {
+                println!("\nError at iteration {}: {:?}", i, e);
+                break;
+            }
+        }
     }
     
     let elapsed = start.elapsed();
     let throughput = (MESSAGE_SIZE * ITERATIONS) as f64 / elapsed.as_secs_f64();
+    
     println!("\nResults:");
     println!("Time: {:?}", elapsed);
     println!("Throughput: {:.2} GB/s", throughput / 1e9);
--- src/net.rs
+++ src/net.rs
@@ -1,28 +1,35 @@
 use tokio::net::{TcpListener, TcpStream};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::time::{timeout, Duration};
 use std::sync::Arc;
 use thiserror::Error;
 
 use crate::RingBuffer;
 
+const TIMEOUT_SECS: u64 = 1;
+
 #[derive(Error, Debug)]
 pub enum NetworkError {
     #[error("IO error: {0}")]
     Io(#[from] std::io::Error),
-    
     #[error("Broker error: {0}")]
     Broker(#[from] crate::BrokerError),
-    
-    #[error("Connection closed")]
-    ConnectionClosed,
+    #[error("Timeout")]
+    Timeout,
 }
 
-/// used for network messages
-///
 #[repr(C, packed)]
 struct MessageHeader {
     size: u32,
-    flags: u32,
+}
+
+impl MessageHeader {
+    fn size(&self) -> u32 {
+        unsafe { 
+            let ptr = (self as *const MessageHeader).cast::<u8>();
+            std::ptr::read_unaligned(ptr.cast::<u32>())
+        }
+    }
 }
 
 pub struct BrokerServer {
@@ -41,7 +48,7 @@ impl BrokerServer {
     pub async fn run(&self) -> Result<(), NetworkError> {
         let addr = format!("0.0.0.0:{}", self.port);
         let listener = TcpListener::bind(&addr).await?;
-        println!("Listening on {}", addr);
+        println!("Server listening on {}", addr);
 
         loop {
             let (socket, addr) = listener.accept().await?;
@@ -59,30 +66,51 @@ impl BrokerServer {
 
 async fn handle_connection(mut socket: TcpStream, ring: Arc<RingBuffer>) -> Result<(), NetworkError> {
     socket.set_nodelay(true)?;
-    
     let mut header_buf = [0u8; std::mem::size_of::<MessageHeader>()];
-    let mut msg_buf = Vec::with_capacity(16 * 1024); 
-    
+    let mut msg_buf = vec![0u8; 1024];  // Start with smaller buffer
+    let mut count = 0;
+
     loop {
-        if socket.read_exact(&mut header_buf).await? == 0 {
-            return Err(NetworkError::ConnectionClosed);
-        }
-        
-        let header = unsafe { 
-            std::ptr::read_unaligned(header_buf.as_ptr() as *const MessageHeader)
-        };
-        
-        // may need to  resize buffer
-        if msg_buf.capacity() < header.size as usize {
-            msg_buf.reserve(header.size as usize);
-        }
-        
-        // read message body
-        unsafe { msg_buf.set_len(header.size as usize) };
-        socket.read_exact(&mut msg_buf).await?;
-        
-        while ring.try_write(&msg_buf).is_err() {
-            tokio::task::yield_now().await;
+        match timeout(Duration::from_secs(TIMEOUT_SECS), socket.read_exact(&mut header_buf)).await {
+            Ok(Ok(_)) => {
+                let header = unsafe {
+                    std::ptr::read_unaligned(header_buf.as_ptr() as *const MessageHeader)
+                };
+                let size = header.size() as usize;
+                println!("Server received header, size: {}", size);
+
+                if msg_buf.len() < size {
+                    msg_buf.resize(size, 0);
+                }
+
+                match timeout(Duration::from_secs(TIMEOUT_SECS), socket.read_exact(&mut msg_buf[..size])).await {
+                    Ok(Ok(_)) => {
+                        while ring.try_write(&msg_buf[..size]).is_err() {
+                            tokio::task::yield_now().await;
+                        }
+                        count += 1;
+                        if count % 10000 == 0 {
+                            println!("Server processed {} messages", count);
+                        }
+                    }
+                    Ok(Err(e)) => {
+                        println!("Server error reading message: {}", e);
+                        return Err(e.into());
+                    }
+                    Err(_) => {
+                        println!("Server timeout reading message");
+                        return Err(NetworkError::Timeout);
+                    }
+                }
+            }
+            Ok(Err(e)) => {
+                println!("Server error reading header: {}", e);
+                return Err(e.into());
+            }
+            Err(_) => {
+                println!("Server timeout reading header");
+                return Err(NetworkError::Timeout);
+            }
         }
     }
 }
@@ -95,24 +123,46 @@ impl BrokerClient {
     pub async fn connect(addr: &str) -> Result<Self, NetworkError> {
         let stream = TcpStream::connect(addr).await?;
         stream.set_nodelay(true)?;
-        
+        println!("Connected to {}", addr);
         Ok(Self { stream })
     }
+
     pub async fn send(&mut self, data: &[u8]) -> Result<(), NetworkError> {
         let header = MessageHeader {
             size: data.len() as u32,
-            flags: 0,
         };
-        
+
         let header_bytes = unsafe {
             std::slice::from_raw_parts(
                 &header as *const _ as *const u8,
                 std::mem::size_of::<MessageHeader>()
             )
         };
-        self.stream.write_all(header_bytes).await?;
-        self.stream.write_all(data).await?;
-        
+
+        match timeout(Duration::from_secs(TIMEOUT_SECS), self.stream.write_all(header_bytes)).await {
+            Ok(Ok(_)) => {},
+            Ok(Err(e)) => {
+                println!("Client error writing header: {}", e);
+                return Err(e.into());
+            }
+            Err(_) => {
+                println!("Client timeout writing header");
+                return Err(NetworkError::Timeout);
+            }
+        }
+
+        match timeout(Duration::from_secs(TIMEOUT_SECS), self.stream.write_all(data)).await {
+            Ok(Ok(_)) => {},
+            Ok(Err(e)) => {
+                println!("Client error writing data: {}", e);
+                return Err(e.into());
+            }
+            Err(_) => {
+                println!("Client timeout writing data");
+                return Err(NetworkError::Timeout);
+            }
+        }
+
         Ok(())
     }
 }
